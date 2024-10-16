d815415c7f98b8fe2272b5ab0a79ba6886de3af9
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2024-10-16 18:37:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+let data ={}
+app.use(express.json());
+app.set("view engine", "ejs");
+app.post("/form-data", (req, res) => {
+  const data = req.body;
+
+  if (data) {
+    const stringifyData = JSON.stringify(data);
+    const removeNewLine = stringifyData.replace(/\\n/g, "");
+    const cleanResultData = removeNewLine.replace(/\\/g, "");
+    console.log(cleanResultData);
+  
+
+    data[cleanResultData['userID']] = cleanResultData ;
+
+   console.log(data);
+
+    res.sendStatus(200);
+  } else {
+    res.status(400).send("Sorry something went wrong");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("I am running on port 3001");
+});
+
+
+app.get("/response/:uuid", (req, res)=>{
+  const uuid = req.params.uuid;
+  if(data[uuid]) {
+    const responseData = JSON.stringify(data[uuid]); 
+    res.json(responseData)
+  } else {
+    res.status(500);
+  }
+
+
+
+
+})
